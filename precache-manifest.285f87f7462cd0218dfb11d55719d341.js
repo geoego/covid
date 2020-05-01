1fe0273fa0a17fe2fc736e2ab158e9afae578cf5
Forrest Cain <forrest.a.cain@gmail.com>
2020-05-01 14:06:47 -0400

    Updates

---
+++ precache-manifest.285f87f7462cd0218dfb11d55719d341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8271145b2202bf9a8d11c518e20aeb4a",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "7e9883cd50359d8427ce",
+    "url": "/covid/static/css/main.eeaf0dca.chunk.css"
+  },
+  {
+    "revision": "8b4c9b63bcc5890f2f19",
+    "url": "/covid/static/js/2.88ad4f35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid/static/js/2.88ad4f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9883cd50359d8427ce",
+    "url": "/covid/static/js/main.276a5f97.chunk.js"
+  },
+  {
+    "revision": "7c8771982593cf4d038c",
+    "url": "/covid/static/js/runtime-main.df4efb12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid/static/media/logo.5d5d9eef.svg"
+  }
+]);
